6650d7baff6638d5ae824d945cf2d6c71cbd6aa9
ito-72 <153911389+ito-72@users.noreply.github.com>
2025-03-29 11:53:37 +0900

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 // /api/config.js
 const GAS_URL
-= "https://script.google.com/macros/s/AKfycbzXq5Nmp9_3NUcjSjj2_R4FUIaK2m1g_9-_Ou7uFT-UZ_ewyA5u4DGOtj5P5fjxYheZ/exec";
+= "https://script.google.com/macros/s/AKfycbz8pkBfS0g8Z-jTewk3qYhZThLrHyyFKOEozIDnBdNXzdL-uMney2HYCuaDwtUxJyxI/exec";
 
 export { GAS_URL };
