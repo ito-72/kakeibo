8e5ca7b3054ceb27aeddff9d37968ca6d29b08a3
ito-72 <153911389+ito-72@users.noreply.github.com>
2025-11-16 21:02:07 +0900

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,7 +1,8 @@
 // /api/config.js
 const GAS_URL
-= "https://script.google.com/macros/s/AKfycbwgz_GPcyllXC50wv0qyJn8se-FA9r_6WFr6mZwdmwDyGMAdDkwupXNVf8G04s2tAkk/exec";
+= "https://script.google.com/macros/s/AKfycbyxjqYYF9cA-K5rnGp_myyzM1oZ62L-HOeMpw1EF-IVQOUSVQvqug3kOamlIOGQ44SJ/exec";
 
 export { GAS_URL };
+
 
 
